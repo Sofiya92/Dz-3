adf46d7c29ea48bd84a7dc2ca9d1d34eead1b563
Sonya <vilinskaya.s@bk.ru>
2023-03-16 22:54:53 +0300

    Добавили решение 19,20,23 задач

---
+++ task19/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 10000 || number > 99999)
+{
+    Console.WriteLine("Неверное число");
+    return;
+}
+
+if (GetDigit(number, 1) == GetDigit(number, 5) && GetDigit(number, 2) == GetDigit(number, 4))
+    Console.WriteLine("Палиндром");
+else
+    Console.WriteLine("Не палиндром");
+
+int GetDigit(int num, int digitNum)
+{
+    return (num / Convert.ToInt32(Math.Pow(10, digitNum-1))) % 10;
+}
---
+++ task23/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+Table(number);
+
+
+void Table(int num)
+{
+    int index = 1;
+    int result = 0;
+    while(index<=num)
+    {
+        result = index*index*index;
+        Console.WriteLine($"| {index,5} | {result,5} |" );
+        index++;
+    }
+}
